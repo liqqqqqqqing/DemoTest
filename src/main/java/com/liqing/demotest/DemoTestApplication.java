81f91c235b02bcdb66491b6069135515e8fcb706
李庆 <liqing@liqingdeMacBook-Pro.local>
2022-09-04 22:12:42 +0800

    添加注释

--- src/main/java/com/liqing/demotest/DemoTestApplication.java
+++ src/main/java/com/liqing/demotest/DemoTestApplication.java
@@ -4,6 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * 好起来了
+ */
 @SpringBootApplication
 public class DemoTestApplication {
 
