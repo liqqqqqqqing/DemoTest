11ff5695e112ce4f9d039760ad13e9642e40cb5e
李庆 <liqing@liqingdeMacBook-Pro.local>
2022-09-05 11:18:02 +0800

    添加拦截器

--- src/main/java/com/liqing/demotest/controller/BaseController.java
+++ src/main/java/com/liqing/demotest/controller/BaseController.java
@@ -5,10 +5,30 @@ import com.liqing.demotest.until.JsonResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.servlet.http.HttpSession;
+
 @RestControllerAdvice
 public class BaseController {
     /** 操作成功的状态码 */
     public static final int OK = 200;
+
+    /**
+     * 从HttpSession对象中获取uid
+     * @param session HttpSession对象
+     * @return 当前登录的用户的id
+     */
+    protected final Integer getUidFromSession(HttpSession session) {
+        return Integer.valueOf(session.getAttribute("uid").toString());
+    }
+
+    /**
+     * 从HttpSession对象中获取用户名
+     * @param session HttpSession对象
+     * @return 当前登录的用户名
+     */
+    protected final String getUsernameFromSession(HttpSession session) {
+        return session.getAttribute("username").toString();
+    }
 
     /** @ExceptionHandler用于统一处理方法抛出的异常 */
     @ExceptionHandler(ServiceException.class)
@@ -29,4 +49,5 @@ public class BaseController {
         }
         return result;
     }
+
 }
